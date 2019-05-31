dcdd53ed15c909661f73e58608a691de00fb298f
Alyssa <amv2187@cumc.columbia.edu>
2019-05-31 11:58:37 -0400

    changed Simulation vignette to have 0.4 unacceptable toxicity rate

--- inst/doc/Simulation_example.R
+++ inst/doc/Simulation_example.R
@@ -1,5 +1,5 @@
 ## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
+knitr::opts_chunk$set(echo = TRUE, warning = F, message = F)
 
 ## ------------------------------------------------------------------------
 library(iAdapt)
@@ -8,10 +8,10 @@ library(iAdapt)
 dose <- 5 
 
 # Vector of true toxicities associated with each dose
-dose.tox <- c(0.05, 0.10, 0.15, 0.20, 0.30)       
+dose.tox <- c(0.05, 0.10, 0.20, 0.35, 0.45)       
 
 # Acceptable (p_yes) and unacceptable (p_no) DLT rates used for establishing safety
-p_no <- 0.25                                     
+p_no <- 0.40                                     
 p_yes <- 0.15    
 
 # Likelihood-ratio (LR) threshold
@@ -34,44 +34,43 @@ stop.rule <- 9
 
 ## ---- echo=F, fig.width=6, fig.height=3----------------------------------
 par(mfrow = c(1,2))
-plot(x = 1:5, y = dose.tox, ylim = c(0, 0.4),
+plot(x = 1:5, y = dose.tox, ylim = c(0, p_no + 0.1),
      type = 'b',
      xlab = "Dose level", ylab = "Toxicity rate",
      main = "(a) Dose-toxicity")
-lines(x = 0:6, y = rep(0.25, 7), lty = 3) # threshold toxicity value
+lines(x = 0:6, y = rep(p_no, 7), lty = 3) # threshold toxicity value
 points(x = 4, y = dose.tox[4], pch = 15, col = "green") # highlight best dose
 
 plot(x = 1:5, y = m, ylim = c(0, 100),
      type = 'b',
      xlab = "Dose level", ylab = "Efficacy",
-     main = "(b) Dose-response")
+     main = "(b) Dose-efficacy")
 points(x = 4, y = m[4], pch = 15, col = "green") # highlight best dose
 
 ## ------------------------------------------------------------------------
-set.seed(2)
+set.seed(3)
 tox.profile(dose = dose, dose.tox = dose.tox, p1 = p_no, p2 = p_yes, K = K, coh.size = coh.size)
 
 ## ------------------------------------------------------------------------
-set.seed(2)
+set.seed(3)
 safe.dose(dose = dose, dose.tox = dose.tox, p1 = p_no, p2 = p_yes, K = K, coh.size = coh.size) 
 
 ## ------------------------------------------------------------------------
-set.seed(2)
+set.seed(3)
 eff.stg1(dose = dose, dose.tox = dose.tox, p1 = p_no, p2 = p_yes, K = K, coh.size = coh.size, m, v, nbb = 100)
 
 ## ------------------------------------------------------------------------
-set.seed(2)
+set.seed(3)
 rand.stg2(dose, dose.tox, p_no, p_yes, K, coh.size, m, v, N, stop.rule = stop.rule, cohort = 1, samedose = T, nbb = 100) 
 
-## ------------------------------------------------------------------------
+## ---- results='hide'-----------------------------------------------------
 numsims = 100
 
-set.seed(2)
+set.seed(3)
 simulations = sim.trials(numsims = numsims, dose, dose.tox, p1 = p_no, p2 = p_yes, K, coh.size, m, v, N, stop.rule = stop.rule, cohort = 1, samedose = T, nbb = 100)
 
-head(simulations$safe.d)
-
 ## ------------------------------------------------------------------------
+head(simulations$safe.d)
 head(simulations$sim.Y)
 head(simulations$sim.d)
 
@@ -85,18 +84,18 @@ m <- c(15, 35, 80, 60, 40)   # MUST BE THE SAME LENGTH AS dose.tox
 
 ## ---- echo=F, fig.width=6, fig.height=3----------------------------------
 par(mfrow = c(1,2))
-plot(x = 1:5, y = dose.tox, ylim = c(0, 0.4),
+plot(x = 1:5, y = dose.tox, ylim = c(0, p_no + 0.1),
      type = 'b',
      xlab = "Dose level", ylab = "Toxicity rate",
      main = "(a) Dose-toxicity")
-lines(x = 0:6, y = rep(0.25, 7), lty = 3) # threshold toxicity value
+lines(x = 0:6, y = rep(p_no, 7), lty = 3) # threshold toxicity value
 points(x = 3, y = dose.tox[3], pch = 15, col = "green") # highlight most effecive dose
 points(x = 4, y = dose.tox[4], pch = 15, col = "red") # highlight MTD
 
 plot(x = 1:5, y = m, ylim = c(0, 100),
      type = 'b',
      xlab = "Dose level", ylab = "Efficacy",
-     main = "(b) Dose-response")
+     main = "(b) Dose-efficacy")
 points(x = 3, y = m[3], pch = 15, col = "green") # highlight best dose
 points(x = 4, y = m[4], pch = 15, col = "red") # highlight MTD
 
@@ -117,15 +116,14 @@ eff.stg1(dose = dose, dose.tox = dose.tox, p1 = p_no, p2 = p_yes, K = K, coh.siz
 set.seed(1)
 rand.stg2(dose, dose.tox, p_no, p_yes, K, coh.size, m, v, N, stop.rule = stop.rule, cohort = 1, samedose = T, nbb = 100) 
 
-## ------------------------------------------------------------------------
+## ---- results='hide'-----------------------------------------------------
 numsims = 100
 
 set.seed(1)
 simulations = sim.trials(numsims = numsims, dose, dose.tox, p1 = p_no, p2 = p_yes, K, coh.size, m, v, N, stop.rule = stop.rule, cohort = 1, samedose = T, nbb = 100)
 
-head(simulations$safe.d)
-
 ## ------------------------------------------------------------------------
+head(simulations$safe.d)
 head(simulations$sim.Y)
 head(simulations$sim.d)
 
